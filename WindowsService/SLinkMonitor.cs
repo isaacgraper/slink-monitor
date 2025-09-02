4565627f16b1fe00d02db4fdf44ce15646ac67d6
isaacgraper <isaackleimmanngraper@gmail.com>
2025-09-02 14:01:54 -0300

    Add project files.

---
+++ WindowsService/SLinkMonitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public partial class SLinkMonitor : ServiceBase
+    {
+        public SLinkMonitor()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+
+            // See current network connections
+
+            // Looping SLink when connected
+            // If SLink is connected, start a timer
+            // If SLink disconnects, stop the timer and log the time
+
+            // Send log to file into C:\SLinkMonitor\Logs or into Google Drive
+
+            
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
